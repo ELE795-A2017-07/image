5ebc32a1983a2e0c19dddffb8d0e9605af983287
asselins <34291342+asselins@users.noreply.github.com>
2017-12-22 18:43:56 -0500

    Update LeavesDetection.cpp

--- LeavesDetection.cpp
+++ LeavesDetection.cpp
@@ -1,5 +1,31 @@
+/** 
+ *  @file    LeavesDetection.cpp
+ *  @author  Samuel Asselin
+ *  @date    19/12/2017 
+ *  @version 1.0 
+ *  
+ *  @brief Detect mildiou, black rot or holes on leaves
+ *
+ *  @section DESCRIPTION
+ *  
+ *  This is a little program that receives an image as input
+ *  and uses thresholding to determine if the leaves contain
+ *	either mildiou or black rot.
+ *	
+ *  TODO: 
+ *		- Do something if there is a problem on the leaves.
+ *		- Detect holes with border detection.
+ *
+ */
+ 
 #include <opencv2/opencv.hpp>
 #include <vector>
+
+#define IMG_WIDTH	300
+#define	IMG_HEIGHT	300
+#define	IMG_FILE	"img_temp.png"
+#define	MSK_FILE	"mask_temp.png"
+#define COMPRESSION_LEVEL	9 // that's compression level, 0 == none, 9 == full
 
 using namespace cv;
 
@@ -12,7 +38,7 @@ int main( int argc, char** argv )
 	
 	std::vector<int> params;
 	params.push_back(CV_IMWRITE_PNG_COMPRESSION);
-	params.push_back(9);   // that's compression level, 9 == full , 0 == none
+	params.push_back(COMPRESSION_LEVEL);   
 	
 	char* imageName = argv[1];
 
@@ -26,25 +52,25 @@ int main( int argc, char** argv )
 	}
 
 	// We resize the image to 300x300
-	Size size(300,300);	
+	Size size(IMG_WIDTH,IMG_HEIGHT);	
 	resize(image,image,size);//resize image
 
 	// Gaussian low-pass filter of size 7x7
 	//GaussianBlur( image1, image1, Size( 7, 7 ), 0, 0 );
 	
 	// We change image space from RGB to HSV
-    	cvtColor(image,hsv,COLOR_BGR2HSV);
+	cvtColor(image,hsv,COLOR_BGR2HSV);
 	
 	// H(0-255) S(0-50) V(0-255)
 	inRange(hsv,Scalar(0,0,0),Scalar(255,50,255),mask);
 	
-	imwrite("mask_temp.png",mask,params);
+	imwrite(MSK_FILE,mask,params);
 	
 	bitwise_not (mask,mask);
 	
 	image.setTo(Scalar(0,0,0),mask);
 	
-	imwrite("img_temp.png",image, params);
+	imwrite(IMG_FILE,image, params);
 	
 	return 0;
 

f2957f1427431789086487dd08a1ed526dbb7e0e
asselins <34291342+asselins@users.noreply.github.com>
2017-12-22 18:34:28 -0500

    Update LeavesDetection.cpp

--- LeavesDetection.cpp
+++ LeavesDetection.cpp
@@ -1,25 +1,51 @@
 #include <opencv2/opencv.hpp>
+#include <vector>
 
 using namespace cv;
 
 int main( int argc, char** argv )
 {
+	
+	Mat image;	// Source image array
+	Mat hsv;
+	Mat mask;
+	
+	std::vector<int> params;
+	params.push_back(CV_IMWRITE_PNG_COMPRESSION);
+	params.push_back(9);   // that's compression level, 9 == full , 0 == none
+	
+	char* imageName = argv[1];
 
- char* imageName = argv[1];
- Mat image;
- image = imread( imageName, IMREAD_COLOR );
+	image = imread( imageName, CV_LOAD_IMAGE_COLOR );
+	
+	// If no image was found
+	if( argc != 2 || !image.data )
+	{
+		printf( " No image was found \n " );
+		return -1;
+	}
 
- // If no image was found
- if( argc != 2 || !image.data )
- {
-   printf( " No image was found \n " );
-   return -1;
- }
- 
- 
- Mat gray_image;
- cvtColor( image, gray_image, COLOR_BGR2GRAY );
- imwrite( "Gray_Image.jpg", gray_image );
- 
- return 0;
+	// We resize the image to 300x300
+	Size size(300,300);	
+	resize(image,image,size);//resize image
+
+	// Gaussian low-pass filter of size 7x7
+	//GaussianBlur( image1, image1, Size( 7, 7 ), 0, 0 );
+	
+	// We change image space from RGB to HSV
+    cvtColor(image,hsv,COLOR_BGR2HSV);
+	
+	// H(0-255) S(0-50) V(0-255)
+	inRange(hsv,Scalar(0,0,0),Scalar(255,50,255),mask);
+	
+	imwrite("mask_temp.png",mask,params);
+	
+	bitwise_not (mask,mask);
+	
+	image.setTo(Scalar(0,0,0),mask);
+	
+	imwrite("img_temp.png",image, params);
+	
+    return 0;
+
 }
